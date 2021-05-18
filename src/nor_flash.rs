55f8505600146158a24e3d52df9796af083fceea
Mathias <mk@blackbird.online>
2021-05-18 10:04:57 +0200

    Change away from iterators

--- src/nor_flash.rs
+++ src/nor_flash.rs
@@ -225,17 +225,11 @@ where
 			if is_subset {
 				// Use `merge_buffer` as allocation for padding `data` to `WRITE_SIZE`
 				let offset = addr as usize % S::WRITE_SIZE;
-				self.merge_buffer[..S::WRITE_SIZE]
-					.iter_mut()
-					.for_each(|c| *c = 0u8);
-				self.merge_buffer[..S::WRITE_SIZE]
-					.iter_mut()
-					.skip(offset)
-					.zip(data)
-					.for_each(|(a, b)| *a = *b);
-				let aligned_addr = addr - offset as u32;
+				let alligned_end = data.len() % S::WRITE_SIZE;
+				self.merge_buffer[..S::WRITE_SIZE].fill(0xff);
+				self.merge_buffer[offset..data.len()].copy_from_slice(data);
 				self.storage
-					.try_write(aligned_addr, &self.merge_buffer[..S::WRITE_SIZE])?;
+					.try_write(addr - offset as u32, &self.merge_buffer[..S::WRITE_SIZE])?;
 			} else {
 				self.storage.try_erase(page.start, page.end())?;
 				self.merge_buffer[..S::ERASE_SIZE]
