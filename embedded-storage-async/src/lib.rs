2145d46d40462ab68e62bf3538f56ff0693b3762
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-01-27 15:04:29 +0100

    Add async version of nor flash
    Add async version of the nor_flash traits, that can be used in an
    async context. In the same way as embedded_hal_async in relation to
    embedded_hal, the traits are stored in a separate crate, with the
    intention of moving to the main crate once the async GAT feature is part
    of Rust stable.

---
+++ embedded-storage-async/src/lib.rs
@@ -0,0 +1,9 @@
+//! # embedded-storage-async - An async Storage Abstraction Layer for Embedded Systems
+//!
+//! Storage traits to allow on and off board storage devices to read and write
+//! data asynchronously.
+
+#![no_std]
+#![feature(generic_associated_types)]
+
+pub mod nor_flash;
