51e59a61349eb9b485acd8c3f6e2434a430bfbb8
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-02-03 09:49:56 +0100

    Move documentation to the trait methods

--- embedded-storage-async/src/nor_flash.rs
+++ embedded-storage-async/src/nor_flash.rs
@@ -31,6 +31,10 @@ pub trait AsyncNorFlash: AsyncReadNorFlash {
 	/// The minumum number of bytes the storage peripheral can erase
 	const ERASE_SIZE: usize;
 
+	type EraseFuture<'a>: Future<Output = Result<(), Self::Error>> + 'a
+	where
+		Self: 'a;
+
 	/// Erase the given storage range, clearing all data within `[from..to]`.
 	/// The given range will contain all 1s afterwards.
 	///
@@ -41,11 +45,11 @@ pub trait AsyncNorFlash: AsyncReadNorFlash {
 	/// Returns an error if the arguments are not aligned or out of bounds (the case where `to >
 	/// from` is considered out of bounds). The implementation can use the [`check_erase`]
 	/// helper function.
-	type EraseFuture<'a>: Future<Output = Result<(), Self::Error>> + 'a
-	where
-		Self: 'a;
 	fn erase<'a>(&'a mut self, from: u32, to: u32) -> Self::EraseFuture<'a>;
 
+	type WriteFuture<'a>: Future<Output = Result<(), Self::Error>> + 'a
+	where
+		Self: 'a;
 	/// If power is lost during write, the contents of the written words are undefined,
 	/// but the rest of the page is guaranteed to be unchanged.
 	/// It is not allowed to write to the same word twice.
@@ -54,8 +58,5 @@ pub trait AsyncNorFlash: AsyncReadNorFlash {
 	///
 	/// Returns an error if the arguments are not aligned or out of bounds. The implementation
 	/// can use the [`check_write`] helper function.
-	type WriteFuture<'a>: Future<Output = Result<(), Self::Error>> + 'a
-	where
-		Self: 'a;
 	fn write<'a>(&'a mut self, offset: u32, bytes: &'a [u8]) -> Self::WriteFuture<'a>;
 }
